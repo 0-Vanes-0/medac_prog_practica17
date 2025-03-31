9105dd1a3866d6db81b5b88158a9c680119be5dc
Ivan <malyshev.ivan@gmail.com>
2025-03-31 11:56:25 +0200

    Loading data from file

--- src/forms/MainForm.java
+++ src/forms/MainForm.java
@@ -1,6 +1,15 @@
 package forms;
 
+import classes.Meme;
+import classes.SaveLoadManager;
+
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableModel;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Author: Ivan + Konstantin
@@ -15,9 +24,25 @@ public class MainForm {
     private JButton deleteButton;
     private JButton fileButton;
     private JButton aboutButton;
+    private JScrollPane tableScrollPane;
 
     public MainForm() {
-
+        List<Meme> memes = SaveLoadManager.loadData();
+        String[][] data = new String[memes.size()][5];
+        for (int i = 0; i < memes.size(); i++) {
+            data[i][0] = memes.get(i).getNombre();
+            data[i][1] = String.valueOf(memes.get(i).getAnyoOrigen());
+            data[i][2] = String.valueOf(memes.get(i).getPopularidad());
+            data[i][3] = memes.get(i).getUrl();
+            data[i][4] = String.valueOf(memes.get(i).isEsImagen());
+        }
+        if (!memes.isEmpty()) {
+            TableModel dataModel = new DefaultTableModel(
+                    data,
+                    new String[] {"Nombre", "Anyo origen", "Popularidad", "url", "Es imagen?"}
+            );
+            elementsTable.setModel(dataModel);
+        }
     }
 
     public static void main(String[] args) {
